c0e274452b0c8c8f053ccc42d3414749c0bf8a9c
Joe Ashton <joe@aswonvos.com>
2025-02-24 10:19:01 +0000

    Configure production settings: disable debug mode and set allowed hosts for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--adl9gnxgymq(7!$#$%xjpz)iu88&=tnpr*#sp3nzvhtx@mehp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com',]
 
 
 # Application definition
