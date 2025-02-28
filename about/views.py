314db9d7b0cffa1170f8c5d5a5801d8ce6992c98
Joe Ashton <joe@aswonvos.com>
2025-02-28 15:00:49 +0000

    Add CollaborateRequest model and form; implement collaboration feature on About page

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
@@ -7,10 +9,19 @@ def about_me(request):
     """
     Renders the About page
     """
+    
+    if request.method == 'POST':
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.success(request, 'Thank you for reaching out! We will get back to you soon.')
+    
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         'about/about.html',
-        {'about': about},
+        {'about': about,
+         "collaborate_form": collaborate_form,},
     )
